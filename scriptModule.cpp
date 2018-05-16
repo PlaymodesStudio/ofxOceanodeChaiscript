6d558db36cf5bebab6f9006909bc9e58d49444cb
Eduard Frigola Bagué <eduardfrigola@yahoo.com>
2018-05-16 13:18:52 +0200

    First commit

---
+++ scriptModule.cpp
@@ -0,0 +1,93 @@
+//
+//  scriptModule.cpp
+//  OceanodeLuaModule_Test
+//
+//  Created by Eduard Frigola on 09/05/2018.
+//
+
+#include "scriptModule.h"
+
+scriptModule::scriptModule() : ofxOceanodeNodeModel("Script Module"){
+    parameters->add(filename.set("Filename", ""));
+    //filename = "button_gate.chai";
+    //fileLastChanged = std::filesystem::last_write_time(ofToDataPath(filename));
+    
+    listeners.push(parameters->parameterChangedE().newListener(this, &scriptModule::parametersListener));
+    
+    chai.add(chaiscript::fun(&scriptModule::addFloatParameter), "addFloatParameter");
+    chai.add(chaiscript::fun(&scriptModule::addIntParameter), "addIntParameter");
+    chai.add(chaiscript::fun(&scriptModule::addBoolParameter), "addBoolParameter");
+    chai.add(chaiscript::fun(&scriptModule::addVoidParameter), "addVoidParameter");
+    chai.add(chaiscript::fun(&scriptModule::addVectorFloatParameter), "addVectorFloatParameter");
+    
+    chai.add(chaiscript::fun(&scriptModule::getFloatParameter), "getFloatParameter");
+    chai.add(chaiscript::fun(&scriptModule::getIntParameter), "getIntParameter");
+    chai.add(chaiscript::fun(&scriptModule::getBoolParameter), "getBoolParameter");
+    chai.add(chaiscript::fun(&scriptModule::getVectorFloatParameter), "getVectorFloatParameter");
+    
+    chai.add(chaiscript::fun(&scriptModule::setFloatParameter), "setFloatParameter");
+    chai.add(chaiscript::fun(&scriptModule::setIntParameter), "setIntParameter");
+    chai.add(chaiscript::fun(&scriptModule::setBoolParameter), "setBoolParameter");
+    chai.add(chaiscript::fun(&scriptModule::setVoidParameter), "setVoidParameter");
+    chai.add(chaiscript::fun(&scriptModule::setVectorFloatParameter), "setVectorFloatParameter");
+    
+    chai.add(chaiscript::fun(&scriptModule::getLastChangedParameterName), "getLastChangedParameterName");
+    
+    chai.add(chaiscript::user_type<scriptModule>(), "scriptModule");
+    chai.add(chaiscript::bootstrap::standard_library::vector_type<vector<float>>("FloatVector"));
+    chai.add(chaiscript::type_conversion<std::vector<chaiscript::Boxed_Value>, std::vector<float>>(
+        [&](const std::vector<chaiscript::Boxed_Value> &t_bvs) {
+            return to_vector_float(t_bvs);
+        }));
+    chai.add_global(chaiscript::var(this), "this");
+    
+    
+    chaiInitState = chai.get_state();
+    try{
+        chai.eval_file(ofToDataPath(filename.get()));
+    }catch (std::exception &e){
+        ofLog() << e.what();
+    }
+    try{
+        listenerFunc = chai.eval<std::function<bool()>>("listenerFunc");
+    }catch (std::exception &e){
+        ofLog() << e.what();
+    }
+    updateParameters();
+}
+
+
+void scriptModule::update(ofEventArgs &a){
+    
+    auto currentLocale = std::filesystem::last_write_time(ofToDataPath(filename.get()));
+    if(currentLocale != fileLastChanged){
+        //parameters->clear();
+        chai.set_state(chaiInitState);
+        try{
+            chai.eval_file(ofToDataPath(filename.get()));
+        }catch (std::exception &e){
+            ofLog() << e.what();
+        }
+        try{
+            listenerFunc = chai.eval<std::function<bool()>>("listenerFunc");
+        }catch (std::exception &e){
+            ofLog() << e.what();
+        }
+        updateParameters();
+        ofNotifyEvent(parameterGroupChanged);
+        fileLastChanged = currentLocale;
+        ofLog() <<"File changed  " <<  ofGetTimestampString();
+    }
+}
+
+//TODO: Have to listen to void parameters separately
+void scriptModule::parametersListener(ofAbstractParameter &param){
+    lastChangedParameterName = param.getName();
+    try{
+        listenerFunc();
+    }
+    catch (std::exception &e){
+        ofLog() << e.what();
+    }
+}
+
---
+++ scriptModule.h
@@ -0,0 +1,147 @@
+//
+//  scriptModule.h
+//  OceanodeLuaModule_Test
+//
+//  Created by Eduard Frigola on 09/05/2018.
+//
+
+#ifndef scriptModule_h
+#define scriptModule_h
+
+#include "ofxOceanodeNodeModel.h"
+#include "chaiscript.hpp"
+#include "chaiscript_stdlib.hpp"
+
+class scriptModule : public ofxOceanodeNodeModel{
+public:
+    scriptModule();
+    ~scriptModule(){};
+    
+    void update(ofEventArgs &a) override;
+
+    virtual void presetSave(ofJson &json) override{
+        ofSerialize(json, filename);
+    };
+    virtual void presetRecallBeforeSettingParameters(ofJson &json) override{
+        ofDeserialize(json, filename);
+    };
+    
+    std::vector<float> to_vector_float(const std::vector<chaiscript::Boxed_Value> &vs) {
+        std::vector<float> vi;
+        std::transform(vs.begin(), vs.end(), std::back_inserter(vi), [](const chaiscript::Boxed_Value &bv) { return chaiscript::Boxed_Number(bv).get_as<float>(); });
+        return vi;
+    }
+    
+    void addFloatParameter(string name, float val, float min, float max){
+        if(!toCreateParameters.contains(name)){
+            auto p = ofParameter<float>();
+            toCreateParameters.add(p.set(name, val, min, max));
+        }
+    }
+    
+    void addIntParameter(string name, int val, int min, int max){
+        if(!toCreateParameters.contains(name)){
+            auto p = ofParameter<int>();
+            toCreateParameters.add(p.set(name, val, min, max));
+        }
+    }
+    
+    void addBoolParameter(string name, bool val){
+        if(!toCreateParameters.contains(name)){
+            auto p = ofParameter<bool>();
+            toCreateParameters.add(p.set(name, val));
+        }
+    }
+    
+    void addVoidParameter(string name){
+        if(!toCreateParameters.contains(name)){
+            auto p = ofParameter<void>();
+            toCreateParameters.add(p.set(name));
+        }
+    }
+    
+    void addVectorFloatParameter(string name, vector<float> val, vector<float> min, vector<float> max){
+        if(!toCreateParameters.contains(name)){
+            auto p = ofParameter<vector<float>>();
+            toCreateParameters.add(p.set(name, val, min, max));
+        }
+    }
+    
+    void updateParameters(){
+        vector<string> parametersNames;
+        for(int i = 0; i < parameters->size(); i++){
+            parametersNames.push_back(parameters->get(i).getEscapedName());
+        }
+        for(int i = 0; i < parametersNames.size(); i++){
+            if(parametersNames[i] != "Filename" && !toCreateParameters.contains(parametersNames[i])){
+                parameters->remove(parametersNames[i]);
+            }
+        }
+        for(int i = 0; i < toCreateParameters.size(); i++){
+            if(!parameters->contains(toCreateParameters.get(i).getName())){
+               parameters->add(toCreateParameters.get(i));
+            }
+        }
+        toCreateParameters.clear();
+    }
+    
+    //Getters
+    float getFloatParameter(string name){
+        return parameters->get(name).cast<float>().get();
+    }
+    
+    int getIntParameter(string name){
+        return parameters->get(name).cast<int>().get();
+    }
+    
+    bool getBoolParameter(string name){
+        return parameters->get(name).cast<bool>().get();
+    }
+    
+    vector<float> getVectorFloatParameter(string name){
+        return parameters->get(name).cast<vector<float>>().get();
+    }
+    
+    //Setters
+    void setFloatParameter(string name, float val){
+        parameters->get(name).cast<float>().set(val);
+    }
+    
+    void setIntParameter(string name, int val){
+        parameters->get(name).cast<int>().set(val);
+    }
+    
+    void setBoolParameter(string name, bool val){
+        parameters->get(name).cast<bool>().set(val);
+    }
+    
+    void setVoidParameter(string name){
+        parameters->get(name).cast<void>().trigger();
+    }
+    
+    void setVectorFloatParameter(string name, vector<float> val){
+        parameters->get(name).cast<vector<float>>().set(val);
+    }
+    
+    string getLastChangedParameterName(){
+        return lastChangedParameterName;
+    }
+    
+private:
+    void parametersListener(ofAbstractParameter &param);
+    
+    ofParameter<string> filename;
+    std::time_t fileLastChanged;
+    
+    chaiscript::ChaiScript chai;
+    chaiscript::ChaiScript::State chaiInitState;
+    
+    std::function<bool()> listenerFunc;
+    string lastChangedParameterName;
+    
+    ofParameterGroup toCreateParameters;
+    
+    ofEventListeners listeners;
+};
+
+#endif /* scriptModule_h */
