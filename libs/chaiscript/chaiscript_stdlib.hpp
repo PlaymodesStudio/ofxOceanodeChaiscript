1c30d081130f8337c34c4d1a0284791db942f64d
Eduard Frigola Bagué <eduardfrigola@gmail.com>
2021-07-24 18:53:13 +0200

    Update to chaiscript 6.1.0

--- libs/chaiscript/chaiscript_stdlib.hpp
+++ libs/chaiscript/chaiscript_stdlib.hpp
@@ -25,14 +25,14 @@
 //#include "dispatchkit/boxed_value.hpp"
 #include "language/chaiscript_prelude.hpp"
 #include "dispatchkit/register_function.hpp"
-//#include "utility/json_wrap.hpp"
+#include "utility/json_wrap.hpp"
 
 #ifndef CHAISCRIPT_NO_THREADS
 #include <future>
 #endif
 
 
-/// @fileg
+/// @file
 ///
 /// This file generates the standard library that normal ChaiScript usage requires.
 
@@ -57,7 +57,7 @@ namespace chaiscript
         lib->add(chaiscript::fun([](const std::function<chaiscript::Boxed_Value ()> &t_func){ return std::async(std::launch::async, t_func);}), "async");
 #endif
 
-        //json_wrap::library(*lib);
+        json_wrap::library(*lib);
 
         lib->eval(ChaiScript_Prelude::chaiscript_prelude() /*, "standard prelude"*/ );
 
